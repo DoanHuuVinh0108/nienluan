a3fdf552acd559177d16e7357235a535e4cd1c13
vinhb2014633@student.ctu.edu.vn <vinhb2014633@student.ctu.edu.vn>
2024-03-12 21:42:51 +0700

    install sequelize

--- src/config/connectionDB.js
+++ src/config/connectionDB.js
@@ -1,15 +1,35 @@
-import mysql from 'mysql2/promise';
+// import mysql from 'mysql2/promise';
 
-// // create the connection to database
+// // // create the connection to database
 
-console.log("Creating connection pool...");
+// console.log("Creating connection pool...");
 
-const pool = mysql.createPool({
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'nienluan',
+//     // password: 'password'
+// })
+
+
+// export default pool;
+
+import Sequelize from 'sequelize';
+
+// Option 1: Passing parameters separately
+const sequelize = new Sequelize('nienluan', 'root', null, {
     host: 'localhost',
-    user: 'root',
-    database: 'nienluan',
-    // password: 'password'
-})
+    dialect: 'mysql',
+    logging: false
+});
 
+let connectDB = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
 
-export default pool;
+module.exports = connectDB;
